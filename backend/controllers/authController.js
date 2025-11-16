41a7987722211ad90e35109d7aa4e5c989224865
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-11-16 13:30:28 +0900

    config the bcrypt hash pass

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,55 @@ const generateToken = (userId) => {
 // @route POST /api/auth/register
 // @access Public
 const registerUser = async (req, res) => {
-    res.status(500).json({ message: "Server error"})
+
+    try {
+        const {
+            name,
+            email,
+            password,
+            profileImageUrl,
+            adminInviteToken
+        } = req.body
+
+        // Check if user already exits
+        const userExists = await User.findOne({ email })
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+
+        // Determine use role: Admin if correct token is provided, otherwise Member
+        let role = "member"
+        if (
+            adminInviteToken &&
+            adminInviteToken == process.env.ADMIN_INVITE_TOKEN
+        ) {
+            role = "admin"
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // Create new user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            profileImageUrl,
+            role,
+        })
+        // Return user data with JWT
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message })
+    }
 }
 
 
@@ -24,6 +72,11 @@ const registerUser = async (req, res) => {
 // @access Private (Requires JWT)
 const loginUser = async (req, res) => {
     
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message })
+    }
 }
 
 // @desc Get user profile
@@ -31,6 +84,11 @@ const loginUser = async (req, res) => {
 // @access Private (Requires JWT)
 const getUserProfile = async (req, res) => {
     
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message })
+    }
 }
 
 
@@ -39,6 +97,11 @@ const getUserProfile = async (req, res) => {
 // @access Private (Requires JWT)
 const updateUserProfile = async (req, res) => {
     
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message })
+    }
 }
 
 export default {
